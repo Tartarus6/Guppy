af39961a4d35f38b59104bb6dd23abcdfea6a8fd
Tartarus6 <ivanglott@gmail.com>
2025-12-03 22:35:08 +0100

    switched to optimistic completed tracking within todo items

--- src/lib/components/TodoItemContainer.svelte
+++ src/lib/components/TodoItemContainer.svelte
@@ -13,7 +13,18 @@
 
     let sectionsContext = getSectionsContext()
 
+    // Optimistic UI state - starts with the todo's actual completed state
+    let optimisticCompleted = $state(props.todo.completed)
+
+    // Update optimistic state when the underlying todo changes
+    $effect(() => {
+        optimisticCompleted = props.todo.completed
+    })
+
     async function handleCheckboxChange(newChecked: boolean) {
+        // Immediately update the UI optimistically
+        optimisticCompleted = newChecked
+        // Then update the server
         await todoService.updateTodo(sectionsContext, props.todo.id, {completed: newChecked})
     }
 </script>
@@ -21,13 +32,13 @@
 <div class='bg-hologram-600 flex flex-row rounded-lg'>
     <div class="mr-3 place-items-center flex">
         <Checkbox 
-            checked={props.todo.completed} 
+            checked={optimisticCompleted} 
             onchange={handleCheckboxChange}
             ariaLabel="Mark todo as complete"
             big={props.big}
         />
     </div>
-    <span class='font-bold {props.big ? 'text-lg' : ''} {props.todo.completed ? 'line-through text-hologram-400' : ''}'>{props.todo.text}</span>
+    <span class='font-bold {props.big ? 'text-lg' : ''} {optimisticCompleted ? 'line-through text-hologram-400' : ''}'>{props.todo.text}</span>
     <DeleteButton onmousedown={async () => {
         await todoService.deleteTodo(sectionsContext, props.todo.id)
     }}></DeleteButton>
